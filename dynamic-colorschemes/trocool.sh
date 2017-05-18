15f2dd0fbdc0b59d2920bbbb394f125ac6a45266
Takrachi <rachel_takla@hotmail.com>
2017-05-18 18:24:13 +0000

    Ajout des colorschemes de dynamic-colors

---
+++ dynamic-colorschemes/trocool.sh
@@ -0,0 +1,25 @@
+# trocool
+
+background="#1c1c1c"
+foreground="#ffffff"
+cursor="#ff0f6b"
+mouse_background=""
+mouse_foreground=""
+highlight=""
+border=""
+color0="#666666" # black
+color1="#fc164c" # red
+color2="#99cc66" # green
+color3="#cc9966" # yellow
+color4="#6699cc" # blue
+color5="#cc6699" # magenta
+color6="#66cccc" # cyan
+color7="#cccccc" # white
+color8="#999999" # brblack
+color9="#ff5995" # brred
+color10="#99ff99" # brgreen
+color11="#ffff99" # bryellow
+color12="#99ccff" # brblue
+color13="#ff99cc" # brmagenta
+color14="#99ffff" # brcyan
+color15="#ffffff" # brwhite
